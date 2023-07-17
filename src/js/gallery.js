a4592c632dc8550b22223d16cf7ca3209708780b
tiramisa <miller19988@gmail.com>
2023-07-17 19:02:20 +0200

    js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,5 +1,5 @@
 import { UnsplashAPI } from './unsplash-api';
-// import createGalleryCards from '../templates/gallery-card.hbs';
+import { CardHelper } from './image_template';
 
 const searchFormEl = document.querySelector('.js-search-form');
 const inputEl = searchFormEl.firstElementChild;
@@ -7,25 +7,42 @@ const galleryListEl = document.querySelector('.js-gallery');
 const loadMoreBtnEl = document.querySelector('.js-load-more');
 
 const unsplashInstance = new UnsplashAPI();
-const handleSearchFormSubmit = event => {
+const cardHelperInstance = new CardHelper();
+const doSearchAndLoad = event => {
   event.preventDefault();
+  loadMoreBtnEl.style.display = "none"
   const searchQuery = inputEl.value.trim();
   if (!searchQuery) {
     return;
+  }
+  if (unsplashInstance.q !== searchQuery) {
+    galleryListEl.innerHTML = ""
+    unsplashInstance.page = 1;
   }
   unsplashInstance.q = searchQuery;
   unsplashInstance
     .fetchPhotos()
     .then(data => {
       console.log(data);
-      if (!data.results.length) {
+      if (!data.hits.length) {
         return console.log('ничего не найдено');
       }
-      galleryListEl.innerHTML = createGalleryCards(data.results);
+      galleryListEl.innerHTML += cardHelperInstance.generatePhotoCard(data.hits);
+      if (data.totalHits / unsplashInstance.itemsPerPage < unsplashInstance.page) {
+        return console.log('вы уже все посмотрели');
+      }
+      loadMoreBtnEl.style.display = "block"
     })
     .catch(console.warn);
 };
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+const handleSearchFormLoadMore = event => {
+  unsplashInstance.page += 1
+  doSearchAndLoad(event)
+};
+
+searchFormEl.addEventListener('submit', doSearchAndLoad);
+loadMoreBtnEl.addEventListener('click', handleSearchFormLoadMore);
 
 // https://pixabay.com/api/?key=38292476-2e9a08398af0b2923a0e3887f&q=yellow+flowers&image_type=photo&pretty=true
 // https://pixabay.com/api/?key=38292476-2e9a08398af0b2923a0e3887f?g=я&page1&per_page=12&client_id38292476-2e9a08398af0b2923a0e3887f
--- src/js/unsplash-api.js
+++ src/js/unsplash-api.js
@@ -6,12 +6,13 @@ export class UnsplashAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   q = null;
   page = 1;
+  itemsPerPage = 40;
   fetchPhotos() {
-    return fetch(
-      `${this.#BASE_URL}?key=38292476-2e9a08398af0b2923a0e3887f&q=${
+    let url = `${this.#BASE_URL}?key=38292476-2e9a08398af0b2923a0e3887f&q=${
         this.q
-      }&page${this.page}&per_page=12&client_id${this.#API_KEY}`
-    ).then(response => {
+      }&page=${this.page}&per_page=${this.itemsPerPage}&client_id=${this.#API_KEY}`;
+    console.log(url);
+    return fetch(url).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
       }
