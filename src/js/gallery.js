5ed0d7a14a5120620858048da44ccd04e36e9133
tiramisa <miller19988@gmail.com>
2023-07-18 01:12:25 +0100

    css

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
 import { UnsplashAPI } from './unsplash-api';
 import { CardHelper } from './image_template';
 
@@ -10,13 +12,13 @@ const unsplashInstance = new UnsplashAPI();
 const cardHelperInstance = new CardHelper();
 const doSearchAndLoad = event => {
   event.preventDefault();
-  loadMoreBtnEl.style.display = "none"
+  loadMoreBtnEl.style.display = 'none';
   const searchQuery = inputEl.value.trim();
   if (!searchQuery) {
     return;
   }
   if (unsplashInstance.q !== searchQuery) {
-    galleryListEl.innerHTML = ""
+    galleryListEl.innerHTML = '';
     unsplashInstance.page = 1;
   }
   unsplashInstance.q = searchQuery;
@@ -25,20 +27,31 @@ const doSearchAndLoad = event => {
     .then(data => {
       console.log(data);
       if (!data.hits.length) {
-        return console.log('ничего не найдено');
+        return Notiflix.Notify.failure(
+          'К сожалению, нет изображений, соответствующих вашему поисковому запросу. Пожалуйста, попробуйте еще раз.'
+        );
+      } else {
+        Notiflix.Notify.success(
+          `Ура, было найдено ${data.totalHits} совпадений`
+        );
       }
-      galleryListEl.innerHTML += cardHelperInstance.generatePhotoCard(data.hits);
-      if (data.totalHits / unsplashInstance.itemsPerPage < unsplashInstance.page) {
-        return console.log('вы уже все посмотрели');
+      galleryListEl.innerHTML += cardHelperInstance.generatePhotoCard(
+        data.hits
+      );
+      if (
+        data.totalHits / unsplashInstance.itemsPerPage <
+        unsplashInstance.page
+      ) {
+        return Notiflix.Notify.failure('Вы дошли до конца списка');
       }
-      loadMoreBtnEl.style.display = "block"
+      loadMoreBtnEl.style.display = 'block';
     })
     .catch(console.warn);
 };
 
 const handleSearchFormLoadMore = event => {
-  unsplashInstance.page += 1
-  doSearchAndLoad(event)
+  unsplashInstance.page += 1;
+  doSearchAndLoad(event);
 };
 
 searchFormEl.addEventListener('submit', doSearchAndLoad);
